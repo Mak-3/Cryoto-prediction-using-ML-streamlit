79f1d89dcef3c4dfc089d0e4fb480746b3f812e1
Mak-3 <75625675+Mak-3@users.noreply.github.com>
2022-04-06 14:38:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email=\"mohammedabdullahkhan26523@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
-headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
